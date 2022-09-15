dd3055ecf4fd5299d220f4f5d8e59c7fb41f6df3
Happy <biswasnedhi09@gmail.com>
2022-09-14 20:16:38 -0500

    completed the studio

---
+++ SpinningDiscs/BaseDisc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpinningDiscs
+{
+    abstract public class BaseDisc
+    {
+        public string Name { get; }
+        public double StorageCapacity { get; }
+        public bool Audio { get; }
+        public bool Video { get; }
+
+        public BaseDisc(string name, double storageCapacity, bool audio, bool video)
+        {
+            Name = name;
+            StorageCapacity = storageCapacity;
+            Audio = audio;
+            Video = video;
+        }
+    }
+}
